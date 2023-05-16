190ec749e212fb058f44759b6d7721001962e2d7
Ogu1208 <kdasunb@naver.com>
2023-05-17 02:51:19 +0900

    [feat]: frames
    properties of frame
    - title
    - DefautlCloseOperation
    - Resizable
    - Size
    - visible
    - ImageIcon
    -BackgroundColor

--- SwingDemo/src/Main.java
+++ SwingDemo/src/Main.java
@@ -1,5 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // JFrame = a GUI window to add components to
+
+        JFrame frame = new JFrame();
+        frame.setTitle("JFrame title goes here");  // sets title of frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // exit out of application
+        frame.setResizable(false);  // prevent frame from being resized
+        frame.setSize(420, 420);  // sets the x-dimension, and y-dimension of frame
+        frame.setVisible(true);  // make frame visible
+
+        ImageIcon image = new ImageIcon("ogu.png"); // create an ImageIcon
+        frame.setIconImage(image.getImage());  // change icon of frame
+        frame.getContentPane().setBackground(new Color(255, 204, 153));  // change color of background
+//        frame.getContentPane().setBackground(new Color(0xFFCC99));
+
     }
 }
